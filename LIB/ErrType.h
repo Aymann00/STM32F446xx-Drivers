7e0ff7cd33d9f79a50be251e747b7d186809ebe6
EngMohammed-Ayman <engmohammedaymann@gmail.com>
2023-07-21 20:31:11 +0300

    Adding Registers File And Errors File with Documentation and Adding Latest Driver's Registers

---
+++ LIB/ErrType.h
@@ -0,0 +1,62 @@
+/*
+ ******************************************************************************
+ * @file           : ErrType.h
+ * @Author         : Mohammed Ayman Shalaby
+ * @brief          : ERROR TYPES FILE
+ * @Date           : May 6, 2023
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 Ayman.
+ * All rights reserved.
+ *
+ ******************************************************************************
+ */
+#ifndef ERRTYPE_H_
+#define ERRTYPE_H_
+
+
+/*==============================================================================================================================================
+ * MANDATORY MACROS
+ *==============================================================================================================================================*/
+#define NULL ((void*)0)
+
+
+typedef enum
+{
+	/*=======================
+	 * GENERAL PURPOSE ERRORS
+	 *=======================*/
+	OK ,
+	NOK ,
+	NULL_POINTER ,
+	/*===========
+	 * RCC ERRORS
+	 *===========*/
+	CLK_STATE_ERROR ,
+	CLK_TYPE_ERROR ,
+	PLL_SRC_ERROR ,
+	TIME_OUT_ERROR ,
+	SYS_CLK_ERROR ,
+	AHB_PRE_ERROR ,
+	APB1_PRE_ERROR ,
+	APB2_PRE_ERROR  ,
+	HSE_CONFIG_ERROR ,
+	PERIPHERAL_ENABLE_ERROR ,
+	PERIPHERAL_DISABLE_ERROR ,
+	BUS_ERROR ,
+	/*============
+	 * GPIO ERRORS
+	 *============*/
+	GPIO_NOK ,
+	GPIO_VALID ,
+	GPIO_NOT_VALID,
+	/*============
+	 * EXTI ERRORS
+	 *============*/
+	EXTI_NOK,
+	EXTI_OK,
+
+}ERRORS_t;
+
+#endif /* ERRTYPE_H_ */
